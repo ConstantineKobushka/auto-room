45cd364b8896ce338be7fd3acf7b1998df6f7590
Constantine Kobushka <kobushkaconstantine@gmail.com>
2025-02-26 10:24:18 +0200

    fixed styles and path to imeges for NotFoundPage

--- src/pages/NotFoundPage/NotFoundPage.jsx
+++ src/pages/NotFoundPage/NotFoundPage.jsx
@@ -6,10 +6,10 @@ const NotFoundPage = () => {
       <picture>
         <source
           media='(max-width: 1440px)'
-          srcSet='not-found-page/not-found-page-bg.jpg 1x, img/not-found-page-bg@2x.jpg 2x'
+          srcSet='/not-found-page/not-found-page-bg.jpg 1x, /not-found-page/not-found-page-bg@2x.jpg 2x'
           type='image/jpg'
         />
-        <img src='not-found-page/not-found-page-bg.jpg' alt='Page not found' />
+        <img src='/not-found-page/not-found-page-bg.jpg' alt='Page not found' />
       </picture>
     </section>
   );
